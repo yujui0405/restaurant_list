40860264f10cced4616963000fa1fcfb8726c7f5
yujui0405 <d27752551@gmail.com>
2022-05-17 02:29:28 +0800

    2022/5/17 實作功能
    加入 restaurant.json 並在app.js require。
    app.js 加入 餐廳詳細資訊頁面router /restaurants/:restaurant_id，使用find()比對json資料。
    加入 view show.handlebars 用來顯示餐廳詳細資料。
    app.js 加入 search 搜尋功能(使用querystring取得判斷參數) router /search。
    app.js 加入 search 功能傳參數給view index ，input value 顯示搜尋輸入。
    view index.handlebars 改成 each 迴圈方式顯示餐廳。

--- app.js
+++ app.js
@@ -3,17 +3,35 @@ const app = express()
 const exphbs = require('express-handlebars')
 const port = 3000
 
+//從json檔載入資料
+const restaurantList = require('./restaurant.json')
+
 //加入樣板引擎
-app.engine('handlebars',exphbs({defaultLayout:'main'}))
-app.set('view engine','handlebars')
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
 
 //set static file
 app.use(express.static('public'))
 
-app.get('/',(req,res)=>{
-  res.render('index')
+app.get('/', (req, res) => {
+  res.render('index', { restaurants: restaurantList.results })
 })
 
-app.listen(port,()=>{
+app.get('/restaurants/:restaurant_id', (req, res) => {
+  const restaurant = restaurantList.results.find(restaurant => req.params.restaurant_id === restaurant.id.toString())
+  // console.log(req.params.id)
+  res.render('show', { restaurant })
+})
+
+//search 功能
+app.get('/search', (req, res) => {
+  //filter 回傳陣列
+  const restaurants = restaurantList.results.filter(restaurant => {
+    return restaurant.name.toLowerCase().includes(req.query.keyword.toLowerCase()) || restaurant.category.toLowerCase().includes(req.query.keyword.toLowerCase())
+  })
+  res.render('index', { restaurants, keyword: req.query.keyword })
+})
+
+app.listen(port, () => {
   console.log(`Express is listening on localhost:${port}`)
 })
