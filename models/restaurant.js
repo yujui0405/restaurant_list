14711d6a50c43ae541cd98035c8505caecec04b2
yujui0405 <d27752551@gmail.com>
2022-05-30 11:48:20 +0800

    feat: 新增models
    安裝mongoose, 加入models資料夾，/models/restaurant.js 匯出RestaurantModel Schema

---
+++ models/restaurant.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const restaurantSchema = new Schema({
+  id: {
+    type: Number,
+    required: true
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  name_en: {
+    type: String
+  },
+  category: {
+    type: String
+  },
+  image: {
+    type: String
+  },
+  location: {
+    type: String
+  },
+  phone: {
+    type: String
+  },
+  google_map: {
+    type: String
+  },
+  rating: {
+    type: Number
+  },
+  description: {
+    type: String
+  }
+})
+
+module.exports = mongoose.model('RestaurantModel', restaurantSchema)
